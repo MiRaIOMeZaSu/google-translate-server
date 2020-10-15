8eecdeeafe53cb48daece09f2b773b060686611b
MiRaIOMeZaSu <49987193+MiRaIOMeZaSu@users.noreply.github.com>
2020-10-15 16:55:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,10 @@
 const translate = require("google-translate-cn-api");
-const stdio = require("stdio");
 const express = require("express");
-const date = require("date-and-time");
 
 const app = express();
-const ops = stdio.getopt({
-  port: { key: "p", args: 1, description: "running port", default: 30031 },
-  domain: {
-    key: "d",
-    args: 1,
-    description: "google translate port",
-    default: "com",
-  },
-});
-const port = parseInt(ops.port);
-const domain = ops.domain;
 
-// log functionalities
-const timeFormat = date.compile("YYYY/MM/DD HH:mm:ss");
-const prettyJson = (object) =>
-  JSON.stringify(object)
-    .replace(/"/g, "")
-    .replace(/([,:])/g, "$1 ");
-function log(req, text, status) {
-  const now = date.format(new Date(), timeFormat);
-  let ip = req.connection.remoteAddress;
-  const params = prettyJson(req.query);
-  console.log(`${now} [${ip}] ${status}  "${text}"  ${params}`);
-}
+const port = process.env.LEANCLOUD_APP_PORT;
+const domain = "com";
 
 // express server
 app.get("/", function (req, res) {
@@ -35,8 +12,8 @@ app.get("/", function (req, res) {
   delete req.query.text;
 
   translate(text, { ...{ domain: domain }, ...req.query })
-    .then((resp) => res.json(resp) && log(req, text, 200))
-    .catch((resp) => res.status(400).json(resp) && log(req, text, 400));
+    .then((resp) => res.json(resp))
+    .catch((resp) => res.status(400).json(resp));
 });
 
 app.listen(port, function () {
@@ -50,5 +27,4 @@ app.listen(port, function () {
   console.log(
     `Default Google Translate domain: https://translate.google.${domain}/`
   );
-  console.log("Incoming requests will be listed below:\n");
 });
